6beb711db9391c3737dcea848fcafeb98d338827
thiagoclrss <thiagocolaress77@gmail.com>
2024-03-21 11:53:37 -0300

    update

--- src/main/java/br/com/taskManager/controller/IndexController.java
+++ src/main/java/br/com/taskManager/controller/IndexController.java
@@ -2,10 +2,14 @@ package br.com.taskManager.controller;
 
 import br.com.taskManager.entity.Task;
 import br.com.taskManager.service.TaskService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -27,5 +31,16 @@ public class IndexController {
         mv.addObject("tasklist", taskList); //esse método atribui a lista de tarefas a palavra tasklist para ser utilizada no html
         return mv;
     }
+
+    @PostMapping
+    public String saveTask(@Valid Task task, BindingResult result, RedirectAttributes redirect){
+
+        if(result.hasErrors()){
+            redirect.addFlashAttribute("mensagem", "Verifique os campos obrigatórios");
+            return "redirec:/task/form/add";
+        }
+        return "redirect:/task/list";
+    }
+
 
 }
--- src/main/java/br/com/taskManager/entity/Task.java
+++ src/main/java/br/com/taskManager/entity/Task.java
@@ -1,20 +1,27 @@
 package br.com.taskManager.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.Date;
 
 @Table(name = "task")
 @Entity (name = "task")
+@Getter
+@Setter
 public class Task {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
     @Column (name = "title")
+    @NotBlank
     private String title;
     @Column (name = "description")
     private String description;
     @Column (name = "expiration")
+    @NotBlank
     private String expiration;
     public Task(String title, String description, String expiration) {
         this.title = title;
